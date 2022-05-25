6b4e8466b9091b1f2f2bd73fb69f0128be9e488b
Max Koch <kocmab18@htl-kaindorf.at>
2022-05-25 16:00:32 +0200

    new Line

--- src/main/java/Tester.java
+++ src/main/java/Tester.java
@@ -2,5 +2,6 @@ public class Tester
 {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        System.out.println("Hello from the other partner");
     }
 }
